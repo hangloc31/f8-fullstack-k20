ec8a559076adcb1a18681cedfc04aef7ab64b784
Hang Loc <hangloc31@gmail.com>
2026-06-16 21:39:44 +0700

    sua baitap3.js

--- day-14/baitap3.js
+++ day-14/baitap3.js
@@ -1,7 +1,7 @@
 function classifyUser(user) {
   const displayName = user.name || "Ẩn danh";
   const isAdult = user.age >= 18;
-  const hasEmail = user.email.length > 0;
+  const hasEmail = user.email ? true : false;
   const role = user.role ?? "guest";
   const status = user.score >= 80 ? "vip" : user.score >= 50 ? "normal" : "new";
 
@@ -18,7 +18,7 @@ function classifyUser(user) {
 const user = {
   name: "Duy",
   age: 16,
-  email: "duy@gmail.com",
+  email: "",
   score: 90,
   role: "admin",
 };
